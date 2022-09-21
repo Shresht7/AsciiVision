b3fb0430dc6488bfcee3bab036a01203d8e3f9df
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-21 15:27:57 +0530

    Create video element to display media stream

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello World!')
+/** @type HTMLVideoElement */
+const video = document.getElementById('video')
+
+/** @type HTMLButtonElement */
+const startBtn = document.getElementById('start')
+/** @type HTMLButtonElement */
+const stopBtn = document.getElementById('stop')
+
+/**
+ * Capture the video stream using the navigator's mediaDevices api
+ * @param {HTMLVideoElement} video Video element to play the stream
+ * @param {boolean} start Start the video stream
+ */
+async function captureVideoStream(video, start = true) {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+    })
+    video.srcObject = stream
+    if (start) { video.play() }
+}
+
+startBtn.addEventListener('click', () => {
+    captureVideoStream(video)
+})
+
+stopBtn.addEventListener('click', () => {
+    video.pause()
+})
