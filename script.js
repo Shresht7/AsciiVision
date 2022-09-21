be6fd709bcfa24e03e21648a9301bfe0c7c90354
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-21 18:48:57 +0530

    Read pixel array and render ascii-video

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 //  CONSTANTS
 //  =========
 
-const WIDTH = 320
-const HEIGHT = 180
+const WIDTH = 120
+const HEIGHT = 90
 
 // =====
 // VIDEO
@@ -21,7 +21,7 @@ async function captureVideoStream(video, start = true) {
     const stream = await navigator.mediaDevices.getUserMedia({
         video: {
             width: WIDTH,
-            height: HEIGHT
+            height: HEIGHT,
         },
         audio: false,
     })
@@ -30,9 +30,9 @@ async function captureVideoStream(video, start = true) {
     draw()
 }
 
-//  ============
-//  VIDEO CANVAS
-//  ============
+//  ======
+//  CANVAS
+//  ======
 
 /** @type HTMLCanvasElement */
 const videoCanvas = document.getElementById('video-canvas')
@@ -80,10 +80,70 @@ function clearCanvas(ctx, width = WIDTH, height = HEIGHT) {
  */
 function renderCanvas(canvas, ctx, image, width = WIDTH, height = HEIGHT) {
     if (!width || !height) { return clearCanvas(ctx) }
-    canvas.width = WIDTH
-    canvas.height = HEIGHT
+    canvas.width = width
+    canvas.height = height
     ctx.drawImage(image, 0, 0, width, height)
 }
+
+/**
+ * Get Pixel Array Data
+ * @param {HTMLCanvasElement} canvas Canvas
+ * @param {CanvasRenderingContext2D} ctx Canvas context
+ */
+function getPixelData(canvas, ctx) {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const data = imageData.data
+
+    const res = []
+    let row = 0, column = 0
+    for (let i = 0; i < data.length; i += 4) {
+        if (!res[row]) { res[row] = new Array(canvas.width) }
+        const r = data[i + 0]
+        const g = data[i + 1]
+        const b = data[i + 2]
+        const a = data[i + 3]
+        res[row][column] = [r, g, b, a]
+        if (column < canvas.width) {
+            column++
+        }
+        if (column === canvas.width) {
+            column = 0
+            row += 1
+        }
+
+    }
+    return res
+}
+
+//  =====
+//  ASCII
+//  =====
+
+const CHARSET = "█▓▒Ñ@#W$9876543210?!abc;:+=-,._                                            ";
+// const CHARSET = '       .:-i|=+%O#@'
+// const CHARSET = '        .:░▒▓█';
+
+const getChar = (scale) => {
+    const val = Math.floor((scale / 255) * (CHARSET.length - 1))
+    let char = CHARSET[val]
+    if (char === " ") { return "&nbsp;" } else { return char }
+}
+
+function renderText(node, data) {
+    let txt = `<div>`
+    for (const row of data) {
+        for (const entry of row) {
+            const [r, g, b, a] = entry
+            const average = (r + g + b) / 3
+            txt += getChar(average)
+        }
+        txt += `<br />`
+    }
+    txt += `</div>`
+    node.innerHTML = txt
+}
+
+const textElement = document.getElementById('ascii-video')
 
 //  ====
 //  DRAW
@@ -92,5 +152,7 @@ function renderCanvas(canvas, ctx, image, width = WIDTH, height = HEIGHT) {
 function draw() {
     if (video.paused) { return }
     renderCanvas(videoCanvas, videoCanvasCtx, video)
+    const data = getPixelData(videoCanvas, videoCanvasCtx)
+    renderText(textElement, data)
     requestAnimationFrame(draw)
 }
